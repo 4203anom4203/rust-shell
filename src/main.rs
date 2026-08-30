16262121da404e0e4c56f1482b2f854f021299a3
Anøm <4203anom4203@gmail.com>
2026-08-29 21:50:17 -0400

    read the input

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,24 @@ use std::io;
 fn main() {
     loop { 
         println!("rsh> ");
-        if let Err(e) = io::Write::flush(&mut io::stdout()) {
+        if let Err(e) = io::Write::flush(&mut io::stdout()) /* if this shit has a problem, throw an error */ {
             eprintln!("Error flushing stdout: {}", e);
+            continue; //reset loop
+        }
+        
+
+        let mut input = String::new();
+        if let Err(e) = io::stdin().read_line(&mut input) {
+            eprintln!("Error reading input: {}", e);
             continue;
         }
+
+        //trim up the enter or \n character
+        let input = input.trim();
+        if input.is_empty() {
+            continue;
+        }
+
         
     }
 }
